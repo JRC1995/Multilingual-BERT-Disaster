f214e0b3f6bf3f097f60eb082c67d4cae0b7c04e
Jishnu Ray Chowdhury <jishnu.ray.c@gmail.com>
2020-05-14 14:04:03 -0500

    Update run_significance.py

--- Test/run_significance.py
+++ Test/run_significance.py
@@ -3,8 +3,6 @@ import numpy as np
 from scipy import stats
 from pathlib import Path
 
-#Functions from : https://github.com/rtmdrr/testSignificanceNLP
-
 
 def calculateContingency(data_A, data_B, n):
     ABrr = 0
@@ -32,7 +30,7 @@ def mcNemar(table):
 alpha = 0.05
 
 model_A = "BERT"
-model_B = "BERT_mixup"
+model_B = "BERT_sent_mixup"
 language = "en"
 times = [0, 1, 2, 3, 4]
 disasters = ["Russia_Meteor", "Cyclone_Pam", "Philipinnes_Floods", "Mixed"]
@@ -57,11 +55,14 @@ for time in times:
         # print(data_A)
         # print(data_B)
 
-        f_obs = calculateContingency(data_A, data_B, len(data_A))
-        mcnemar_results = mcNemar(f_obs)
+        #f_obs = calculateContingency(data_A, data_B, len(data_A))
+        #pval = mcNemar(f_obs)
 
-        significant = (mcnemar_results <= alpha)
+        results = stats.ttest_rel(data_A, data_B)
+        pval = float(results[1]) / 2
+
+        significant = (pval <= alpha)
 
         print("Model A: {}, Model B: {} | Disaster: {} | P-val: {} | Significant: {}".format(
-            model_A, model_B, disaster, mcnemar_results, significant))
+            model_A, model_B, disaster, pval, significant))
     print("\n\n")
